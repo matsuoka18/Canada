5b35434fa8b8a1040668e44f3937614e628a2daa
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-08-08 08:32:48 +0900

    Add files via upload

--- js-video.js
+++ js-video.js
@@ -4,23 +4,41 @@ function start(){
     if (size < 760) {
 
         location.href = "video2.html";
+        ani();
     } else {
       return;
     }
 }
+
+function start2(){
+    document.getElementById("div11").style.display="none";
+    document.getElementById("div11").style.opacity=0;
+    document.getElementById("div1").style.display="flex";
+    document.getElementById("div1").style.opacity=1;
+}
+function start3(){
+    document.getElementById("p1").style.fontWeight = "600";
+    document.getElementById("p2").style.fontWeight = "500";
+    document.getElementById("div11").style.display="none";
+    document.getElementById("div11").style.opacity=0;
+    document.getElementById("div1").style.display="flex";
+    document.getElementById("div1").style.opacity=1;
+}
+function start4(){
+    document.getElementById("p2").style.fontWeight = "600";
+    document.getElementById("p1").style.fontWeight = "500";
+    document.getElementById("div11").style.display="none";
+    document.getElementById("div11").style.opacity=0;
+    document.getElementById("div1").style.display="flex";
+    document.getElementById("div1").style.opacity=1;
+}
 function p1(){
     document.getElementById("p1").style.fontWeight = "600";
     document.getElementById("p2").style.fontWeight = "500";
-    document.getElementById("ca").style.display = "block";
-    document.getElementById("ca").style.opacity = 1;
-    document.getElementById("jp").style.display = "none";
-    document.getElementById("jp").style.opacity = 0;
+    location.href="canada2.html";
 }
 function p2(){
     document.getElementById("p2").style.fontWeight = "600";
     document.getElementById("p1").style.fontWeight = "500";
-    document.getElementById("jp").style.display = "block";
-    document.getElementById("jp").style.opacity = 1;
-    document.getElementById("ca").style.display = "none";
-    document.getElementById("ca").style.opacity = 0;
+    location.href="japan2.html";
 }
