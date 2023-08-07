6885d66fe828c69d4616193b8c2fedd63f2ee11e
matsuoka18 <82459435+matsuoka18@users.noreply.github.com>
2023-08-07 14:27:19 +0900

    Add files via upload

--- js-video.js
+++ js-video.js
@@ -1,6 +1,16 @@
 function p1(){
-    
+    document.getElementById("p1").style.fontWeight = "600";
+    document.getElementById("p2").style.fontWeight = "500";
+    document.getElementById("ca").style.display = "block";
+    document.getElementById("ca").style.opacity = 1;
+    document.getElementById("jp").style.display = "none";
+    document.getElementById("jp").style.opacity = 0;
 }
 function p2(){
     document.getElementById("p2").style.fontWeight = "600";
+    document.getElementById("p1").style.fontWeight = "500";
+    document.getElementById("jp").style.display = "block";
+    document.getElementById("jp").style.opacity = 1;
+    document.getElementById("ca").style.display = "none";
+    document.getElementById("ca").style.opacity = 0;
 }
